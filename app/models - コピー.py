2f4b11251b58c7210c31d3fab357f43cd513a12c
Fumito ONUMA <avocado2310@gmail.com>
2021-05-08 22:33:57 +0900

    My Django Girls app, first commit

---
+++ app/models - コピー.py
@@ -0,0 +1,83 @@
+from django.db import models
+
+from users.models import User
+
+
+class Item(models.Model):
+    """
+    データ定義クラス
+      各フィールドを定義する
+    参考：
+    ・公式 モデルフィールドリファレンス
+    https://docs.djangoproject.com/ja/2.1/ref/models/fields/
+    """
+
+    # サンプル項目7 日付
+    sample_7 = models.DateField(
+        verbose_name='サンプル項目7 日付',
+        blank=True,
+        null=True,
+    )
+22
+    # サンプル項目6 ブール値
+    sample_6 = models.BooleanField(
+        verbose_name='朝食',
+    )
+
+"""     # サンプル項目6 ブール値
+    sample_6 = models.BooleanField(
+        verbose_name='夕食',
+    ) """
+
+    # 以下、管理項目
+
+    # 作成者(ユーザー)
+    created_by = models.ForeignKey(
+        User,
+        verbose_name='作成者',
+        blank=True,
+        null=True,
+        related_name='CreatedBy',
+        on_delete=models.SET_NULL,
+        editable=False,
+    )
+
+    # 作成時間
+    created_at = models.DateTimeField(
+        verbose_name='作成時間',
+        blank=True,
+        null=True,
+        editable=False,
+    )
+
+    # 更新者(ユーザー)
+    updated_by = models.ForeignKey(
+        User,
+        verbose_name='更新者',
+        blank=True,
+        null=True,
+        related_name='UpdatedBy',
+        on_delete=models.SET_NULL,
+        editable=False,
+    )
+
+    # 更新時間
+    updated_at = models.DateTimeField(
+        verbose_name='更新時間',
+        blank=True,
+        null=True,
+        editable=False,
+    )
+
+    def __str__(self):
+        """
+        リストボックスや管理画面での表示
+        """
+        return self.sample_1
+
+    class Meta:
+        """
+        管理画面でのタイトル表示
+        """
+        verbose_name = 'サンプル'
+        verbose_name_plural = 'サンプル'
